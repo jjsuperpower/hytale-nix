0b373d920f2af15ca6a0dd47dfa67668d5afe0b3
andreashgk <andreas@hgk.me>
2026-01-10 19:46:36 +0100

    chore: bump to `2026.01.10-5fdaa5c`

--- flake.nix
+++ flake.nix
@@ -12,11 +12,11 @@
       packages.x86_64-linux.default = self.packages.x86_64-linux.hytale-launcher;
       packages.x86_64-linux.hytale-launcher = pkgs.stdenv.mkDerivation rec {
         pname = "hytale-launcher";
-        version = "2026.01.10-980a9ef";
+        version = "2026.01.10-5fdaa5c";
 
         src = pkgs.fetchzip {
           url = "https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-${version}.zip";
-          sha256 = "sha256-kf4TNvtwBb8sF1Sty7wbNzLiQl3McBXiM+2bFWfAT9c=";
+          sha256 = "sha256-cnFgrn4YW4SuAvmlPfuW0lGXziFat61W+tj1LoQbsOQ=";
         };
 
         nativeBuildInputs = [ pkgs.autoPatchelfHook ];
